82067032999a2602c9f8c81230c9385271c4a3c7
Tomas Prokop <xprokopt@gmail.com>
2016-11-21 21:46:33 +0100

    First commit project Xamarin

---
+++ BitcoinWallet/BitcoinWallet/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace BitcoinWallet.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
