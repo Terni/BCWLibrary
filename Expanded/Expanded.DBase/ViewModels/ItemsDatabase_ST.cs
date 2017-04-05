bb7e471f4ee1948e0264f888f9312505cb9a4b8e
Tomas Prokop <@gmail.com>
2017-04-05 22:53:02 +0200

    add missing files *.cs

---
+++ Expanded/Expanded.DBase/ViewModels/ItemsDatabase_ST.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Expanded.DBase.Models;
+using SQLite;
+
+namespace Expanded.DBase.ViewModels
+{
+    public static class ItemsDatabase_ST
+    {
+        /// <summary>
+        /// Variables
+        /// </summary>
+        static SQLiteAsyncConnection _database;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="dbPath">Specific path into file database</param>
+        static ItemsDatabase_ST()
+        {
+            _database = null;
+        }
+
+
+        public static SQLiteAsyncConnection DatabaseString
+        {
+            get
+            {
+                return _database;
+            }
+            set
+            {
+                _database = value;
+            }
+        }
+
+
+
+    }
+}
