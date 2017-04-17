96555d5dae3441a2dcf17a252ac83944b542886e
Tomas Prokop <@gmail.com>
2017-04-17 17:36:00 +0200

    bad code in ifs RatersPinMap

--- Bitcoin/Bitcoin.APIv2Client/ViewModels/RatersPinMap.cs
+++ Bitcoin/Bitcoin.APIv2Client/ViewModels/RatersPinMap.cs
@@ -32,31 +32,33 @@ namespace Bitcoin.APIv2Client.ViewModels
                         Longitude = (double)obj["long"],
                         Type = (string)obj["type"],
                         isTowway = (bool)obj["is_twoway"],
-                        //Cryptos = CreateCryptosTList((string)obj["cryptos"]),
+                        Cryptos = CreateCryptosTList((JObject)obj["cryptos"]),
                         Url = (Uri)obj["url"]
                     };
                 datalist.Add(item);
             } 
-            //var values = JObject.Parse(jsonData).Property("values").Value.AsEnumerable();
-            //var data =
-            //    values.Select(
-            //        d =>
-            //            new DataPointChart
-            //            {
-            //                Date = d.Value<long>("x").ConvertFromUnixTimeStamp(),
-            //                Value = d.Value<float>("y")
-            //            }).ToList();
 
             return datalist;
         }
 
-        private static List<CryptosType> CreateCryptosTList(string items)
+        /// <summary>
+        /// Method for create list <see cref="CryptosType"/> from one line in items
+        /// </summary>
+        /// <param name="items">Specific line for json</param>
+        /// <returns>Result is list <see cref="CryptosType"/></returns>
+        private static List<CryptosType> CreateCryptosTList(JObject items)
         {
             List<CryptosType> result = new List<CryptosType>();
-            var data = JObject.Parse(items);
-            foreach (var it in data.Properties().Select(p => p.Value))
+            foreach (var it in items)
             {
-                result.Add(it.Value<CryptosType>());
+                if (it.Key == CryptosType.bitcoin.ToString() && it.Value.ToString() == "1")
+                    result.Add(CryptosType.bitcoin);
+                if (it.Key == CryptosType.dogecoin.ToString() && it.Value.ToString() == "1")
+                    result.Add(CryptosType.dogecoin);
+                if (it.Key == CryptosType.litecoin.ToString() && it.Value.ToString() == "1")
+                    result.Add(CryptosType.litecoin);
+                if (it.Key == CryptosType.ether.ToString() && it.Value.ToString() == "1")
+                    result.Add(CryptosType.ether);
             }
 
             return result;
