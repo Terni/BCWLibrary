d6033e967b1aacccf841ca60ce3696e8b6488e71
Tomas Prokop <@gmail.com>
2017-04-19 03:17:40 +0200

    Add class for Client ApiV2

---
+++ Bitcoin/Bitcoin.APIv2Client/ViewModels/ClientApi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Info.Blockchain.API.Abstractions;
+using Info.Blockchain.API.Wallet;
+using Info.Blockchain.API;
+using Info.Blockchain.API.BlockExplorer;
+
+
+namespace Bitcoin.APIv2Client.ViewModels
+{
+    public class ClientApi
+    {
+        private readonly BlockchainApiHelper _blockchainApi;
+        private Uri BaseUrl { get; set; }
+        private string ApiCode { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="apiCode">Specific apicode after create base url</param>
+        /// <param name="serviceUrl">Specific for service url</param>
+        public ClientApi(string apiCode, Uri serviceUrl = null)
+        {
+            ApiCode = apiCode;
+            BaseUrl = serviceUrl;
+            if (serviceUrl == null)
+            {
+                _blockchainApi = new BlockchainApiHelper(ApiCode);
+            }
+            else
+            {
+                // Only pro service = Create new wallet
+                _blockchainApi = new BlockchainApiHelper(ApiCode, null, BaseUrl.AbsolutePath);
+            }
+        }
+
+        /// <summary>
+        /// Method for set your wallet
+        /// </summary>
+        /// <param name="idwallet">Specific identifier current wallet</param>
+        /// <param name="pass">Decrypted first password</param>
+        /// <param name="passtwo">Decrypted second password</param>
+        public WalletHelper GetWallet(string idwallet, string pass, string passtwo = null)
+        {
+            return _blockchainApi.CreateWalletHelper(identifier: idwallet, password: pass, secondPassword: passtwo);
+        }
+
+    }
+}
---
+++ BitcoinWallet/BitcoinWallet/ViewModels/Logon.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Info.Blockchain.API;
+using Info.Blockchain.API.Wallet;
+using Bitcoin.APIv2Client.ViewModels;
+using Info.Blockchain.API.BlockExplorer;
+
+namespace BitcoinWallet.ViewModels
+{
+    public class Logon
+    {
+        public static string ApiCode { get; set; }
+        public static string IdentifierWallet { get; set; }
+        public static string Password { get; set; }
+        public static string PasswordSecond { get; set; }
+
+        /// <summary>
+        /// Property pro <see cref="WalletHelper"/>
+        /// </summary>
+        public WalletHelper Wallet { get; set; }
+
+        public BitcoinValue Balance { get; set; }
+        public Info.Blockchain.API.Wallet.Address Address { get; set; }
+
+        public Logon()
+        {
+            ClientApi _client = new ClientApi(ApiCode);
+            Wallet = _client.GetWallet(IdentifierWallet, Password, PasswordSecond);
+
+            
+        }
+
+        //public async Task<BitcoinValue> GetBalance()
+        //{
+        //    return await Wallet.GetBalanceAsync();
+        //}
+
+        //public async Task<Info.Blockchain.API.Wallet.Address> GetAddress(string address)
+        //{
+        //    return await Wallet.GetAddressAsync(address);
+        //}
+
+        //public async Task<PaymentResponse> SendMoney(string toAddress, BitcoinValue amount, string fromAddress = null, BitcoinValue? fee = null, string note = null)
+        //{
+        //    return await Wallet.SendAsync(toAddress, amount, fromAddress, fee, note);
+        //}
+
+    }
+}
