390f2a0cbd5c82f6d3ecc78bcf2eb09b1969d82f
Tomas Prokop <@gmail.com>
2017-04-19 22:34:56 +0200

    add comments and ViewTransaction model

--- Bitcoin/Bitcoin.APIv2Client/Models/TypeApi.cs
+++ Bitcoin/Bitcoin.APIv2Client/Models/TypeApi.cs
@@ -8,15 +8,41 @@ namespace Bitcoin.APIv2Client.Models
 {
     public static class BaseApi
     {
+        public static readonly string BaseName = "https://blockchain.info/";
         public static readonly string ApiName = "https://api.blockchain.info/";
-        public static readonly string MarketName = "https://blockchain.info/";
         public static readonly string MapsApiName = "https://coinatmradar.com/api/locations/1990-01-01/";
+
+        /// <summary>
+        /// Examples:
+        /// 
+        /// Charts = https://api.blockchain.info/charts/xxxxx?timespan=5weeks&rollingAverage=8hours&format=json
+        ///        = xxxx is specific name for chart
+        ///        = https://api.blockchain.info/charts/transactions-per-second?timespan=5weeks&rollingAverage=8hours&format=json
+        ///        = https://api.blockchain.info/charts/market-price?timespan=5weeks&rollingAverage=8hours&format=json
+        ///        = all is https://blockchain.info/charts
+        /// 
+        /// History = https://blockchain.info/address/$bitcoin_address?format=json
+        /// 
+        /// Maps = https://coinatmradar.com/api/locations/ 
+        /// Maps = https://coinatmradar.com/api/locations/<date_from>/ 
+        /// Maps = https://coinatmradar.com/api/locations/<date_from>/<date_to>/ 
+        /// 
+        /// Ticker = https://blockchain.info/ticker
+        /// 
+        /// Stats = https://api.blockchain.info/stats
+        /// 
+        /// Pools = https://api.blockchain.info/pools?timespan=5days
+        ///       = max 10 days
+        /// </summary>
         public enum Type
         {
             charts = 0,
             stats = 1,
             pools = 2,
-            ticker = 3
+            ticker = 3,
+            address = 4
         }
+
+        
     }
 }
