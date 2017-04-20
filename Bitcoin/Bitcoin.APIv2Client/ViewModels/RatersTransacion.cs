5360638c0eceb3d6d682722091b08dbd1a8de2f9
Tomas Prokop <@gmail.com>
2017-04-21 01:03:51 +0200

    add change for RatersTransacion

--- Bitcoin/Bitcoin.APIv2Client/ViewModels/RatersTransacion.cs
+++ Bitcoin/Bitcoin.APIv2Client/ViewModels/RatersTransacion.cs
@@ -25,24 +25,95 @@ namespace Bitcoin.APIv2Client.ViewModels
 
             foreach (var obj in data.Properties().Select(p => p.Value))
             {
-                //TODO: Tady dodelat kod pro ziskani hodnot 
-
-                var item =  new DataTransaction
+                var item = new DataTransaction
                 {
-                        Latitutde = (double)obj["lat"],
-                        Longitude = (double)obj["long"],
-                        Type = (string)obj["type"],
-                        isTowway = (bool)obj["is_twoway"],
-                        //Cryptos = CreateCryptosTList((JObject)obj["cryptos"]),
-                        Url = (Uri)obj["url"]
+                        Hash160 = (string)obj["hash160"],
+                        Address = (string)obj["address"],
+                        NumberTransaction = (int)obj["n_tx"],
+                        TotalRecived = (long)obj["total_received"],
+                        TotalSent = (long)obj["total_sent"],
+                        TotalBalance = (long)obj["total_balance"],
+                        ListTransactions = GetListTransaction((JObject)obj["txs"])
                     };
-                datalist.Add(item);
+                datalist = item;
             } 
 
             return datalist;
         }
 
+        private static List<Transaction> GetListTransaction(JObject trans)
+        {
+            List<Transaction> listTransactions = new List<Transaction>();
+            foreach (var oneTx in trans)
+            {
+                //TODO dopsat
 
+                var input = new InputRow
+                {
+
+                };
+                var item = new Transaction
+                {
+                    ListInputs = new List<InputRow>(),
+                    TupleOuts = new Tuple<OutRow, OutRow>(new OutRow(), new OutRow())
+                };
+
+
+            }
+            return listTransactions;
+        }
+
+        //public class OutRow
+        //{
+        //    public bool Spent { get; set; }
+        //    public long TxIndex { get; set; }
+        //    public int Type { get; set; }
+        //    public string Address { get; set; }
+        //    public long Value { get; set; }
+        //    public int Number { get; set; } //n
+        //    public string Script { get; set; }
+        //}
+
+        //public class InputRow
+        //{
+        //    public long Sequence { get; set; }
+        //    public OutRow PrevOut { get; set; }
+        //    public string Script { get; set; }
+        //}
+
+        //public class Transaction
+        //{
+        //    public int Ver { get; set; }
+        //    public List<InputRow> ListInputs { get; set; }
+        //    public long BlockHeight { get; set; }
+        //    public string RelayedBy { get; set; }
+        //    public Tuple<OutRow, OutRow> TupleOuts { get; set; }
+        //    public string LockTime { get; set; }
+        //    public double Result { get; set; }
+        //    public long Size { get; set; }
+        //    public string Time { get; set; }
+        //    public long TxIndex { get; set; }
+        //    public int VinSz { get; set; }
+        //    public string Hash { get; set; }
+        //    public int VoutSz { get; set; }
+        //}
+
+        //private static List<CryptosType> CreateCryptosTList(JObject items)
+        //{
+        //    List<CryptosType> result = new List<CryptosType>();
+        //    foreach (var it in items)
+        //    {
+        //        if (it.Key == CryptosType.bitcoin.ToString() && it.Value.ToString() == "1")
+        //            result.Add(CryptosType.bitcoin);
+        //        if (it.Key == CryptosType.dogecoin.ToString() && it.Value.ToString() == "1")
+        //            result.Add(CryptosType.dogecoin);
+        //        if (it.Key == CryptosType.litecoin.ToString() && it.Value.ToString() == "1")
+        //            result.Add(CryptosType.litecoin);
+        //        if (it.Key == CryptosType.ether.ToString() && it.Value.ToString() == "1")
+        //            result.Add(CryptosType.ether);
+        //    }
+        //    return result;
+        //}
 
     }
 }
